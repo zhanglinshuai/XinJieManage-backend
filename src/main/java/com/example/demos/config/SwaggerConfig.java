74dcaabee02e79ad976c3693a951091137d45e72
zls <1216486490@qq.com>
2024-04-17 14:57:43 +0800

    整合Swagger+knife4j在线开发文档

---
+++ src/main/java/com/example/demos/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.example.demos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                // apiInfo()：配置 API 的一些基本信息，比如：文档标题title，文档描述description，文档版本号version
+                .apiInfo(apiInfo())
+                // select()：生成 API 文档的选择器，用于指定要生成哪些 API 文档
+                .select()
+                // apis()：指定要生成哪个包下的 API 文档
+                .apis(RequestHandlerSelectors.basePackage("com.example.demos.controller"))
+                // paths()：指定要生成哪个 URL 匹配模式下的 API 文档。这里使用 PathSelectors.any()，表示生成所有的 API 文档。
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private static final String API_TILE="心界智控管理系统";
+    //文档信息配置
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 文档标题
+                .title(API_TILE)
+                // 文档描述信息
+                .description("心界智控在线API开发文档")
+                // 文档版本号
+                .version("1.0")
+                .build();
+    }
+}
